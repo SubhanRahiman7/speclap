fb3078b9f7d2eb406961134c777a05e71641df6a
Subhan Rahiman <subhanrahiman7@gmail.com>
2025-10-21 18:32:22 +0530

    feat: integrate Clerk for user authentication and Inngest for event handling
    - Added @clerk/clerk-react to frontend dependencies for user authentication.
    - Updated App component to include Clerk's SignedIn and SignedOut components for user session management.
    - Removed unused assets and styles related to previous implementation.
    - Created user model in backend to store user information from Clerk.
    - Implemented Inngest functions to sync user data on creation and deletion events from Clerk.
    - Updated server configuration to use Inngest for handling API events.
    - Refactored environment configuration to properly load API keys from environment variables.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,36 @@
-import express from "express"
-import {ENV} from "./config/env.js"
+import express from "express";
+import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./src/inngest"
 
 const app = express();
+ 
+app.use(express.json());
+app.use(clerkMiddleware());
 
-app.get('/', (req, res) => {
-    res.json({message: "Hello World"})
-})
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.listen(ENV.PORT, () => {{
-    console.log("Server started on http://localhost:"+ ENV.PORT)
-    connectDB()
-}})
+app.get("/", (req, res) => {
+    res.json({ message: "Hello World" });
+});
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        if (ENV.NODE_ENV !== "production") {
+            app.listen(ENV.PORT, () => {
+                console.log("Server is listening on http://localhost:", ENV.PORT)
+            })
+        }
+    } catch(error) {
+        console.error("Error starting the server: ", error);
+        process.exit(1);
+    }
+};
+
+startServer();
+
+export default app;
+
