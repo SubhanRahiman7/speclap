62a995a0269b8befb1bcd8789f902c187d2d487d
Subhan Rahiman <subhanrahiman7@gmail.com>
2025-10-21 20:11:16 +0530

    feat: add Inngest functions for user creation and deletion events

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
-import {ENV} from "./env.js";
+import { ENV } from "./env.js";
 
 export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(ENV.MONGO_URI);
-        console.log("MongoDB connected successfully:", conn.connection.host);
-    } catch (error) {
-        console.log("Error connecting to MongoDB:", error);
-        process.exit(1)
-    }
-}
+  try {
+    const conn = await mongoose.connect(ENV.MONGO_URI);
+
+    console.log("MongoDB connected successfully:", conn.connection.host);
+  } catch (error) {
+    console.log("Error connecting to MongoDB:", error);
+    process.exit(1); 
+  }
+};
