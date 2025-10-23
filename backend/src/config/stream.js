d97221736305b6f59be08b79ce84ceec3826afcd
Subhan Rahiman <subhanrahiman7@gmail.com>
2025-10-24 01:43:35 +0530

    feat: integrate Stream Chat API for user management
    - Updated package.json to include @sentry/node and stream-chat dependencies.
    - Added chat.route.js for handling chat-related routes, including token generation.
    - Enhanced inngest.js to upsert and delete Stream users during user sync and deletion.
    - Created stream.js for Stream Chat user management functions.
    - Modified server.js to include chat routes and Sentry error handling.

---
+++ backend/src/config/stream.js
@@ -0,0 +1,34 @@
+import {StreamChat} from "stream-chat";
+import {ENV} from "../config/env.js";
+
+const StreamClient = StreamChat.getInstance(ENV.STREAM_API_KEY, ENV.STREAM_API_SECRET);
+
+export const upsertStreamUser = async (userData) => {
+    try {
+        await StreamClient.upsertUser(userData);
+        console.log("Stream user upserted successfully:", userData.name);
+        return userData;
+    } catch (error) {
+        console.error("Error upserting Stream user:", error);
+    }
+};
+
+export const deleteStreamUser = async (userId) => {
+    try {
+        await StreamClient.deleteUser(userId);
+        console.log("Stream user deleted successfully:", userId);
+    } catch (error) {
+        console.error("Error deleting Stream user:", error);
+    }
+};
+
+export const generateStreamToken = (userId) => {
+    try {
+        const userIdString = userId.toString();
+        return StreamClient.createToken(userIdString);
+    } catch (error) {
+        console.error("Error generating Stream token:", error);
+        return null;
+    }
+}
+
