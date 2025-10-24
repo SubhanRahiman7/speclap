76d85aa286d5c6da80f5852d9634ecfd0b0f3dbb
Subhan Rahiman <subhanrahiman7@gmail.com>
2025-10-24 23:00:24 +0530

    integrate Sentry for error tracking, add axios instance for API calls, and implement AuthProvider for token management
    - Added Sentry for error tracking in the application.
    - Created a new axios instance with a base URL for API requests.
    - Implemented AuthProvider to manage authentication tokens and set up axios interceptors.
    - Updated package.json to include new dependencies: @sentry/react, @tanstack/react-query, axios, react-hot-toast, stream-chat, and stream-chat-react.
    - Modified App component to use Sentry's routing and added QueryClientProvider for React Query.
    - Adjusted styles in auth.css for improved responsiveness of image containers.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,25 @@
+import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ClerkProvider } from '@clerk/clerk-react'
-import { BrowserRouter } from "react-router"
+import { ClerkProvider } from "@clerk/clerk-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+import AuthProvider from "./providers/AuthProvider.jsx";
+import * as Sentry from "@sentry/react";
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes
+} from "react-router"
+
+
+const queryClient = new QueryClient();
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -12,11 +28,30 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
+Sentry.init({
+  dsn: "https://0a28a9e1d4dbaaf883abf83eb15842ee@o4510223325003776.ingest.us.sentry.io/4510245534367744",
+  integrations: [ 
+    Sentry.reactRouterV7BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+          <Toaster position="top-right" />
+        </QueryClientProvider>
       </BrowserRouter>
     </ClerkProvider>
   </StrictMode>
