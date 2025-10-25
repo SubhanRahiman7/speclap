e3a8e06860504de450cfd719c55247916a0c82ef
Subhan Rahiman <subhanrahiman7@gmail.com>
2025-10-25 16:00:29 +0530

    fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,28 @@
-import "../instrument.js"
+import "../instrument.mjs";
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
+import { functions, inngest } from "./config/inngest.js";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./config/inngest.js"
-import chatRoutes from "../routes/chat.route.js";
-import * as Sentry from "@sentry/node";
+import chatRoutes from "./routes/chat.route.js";
+
 import cors from "cors";
 
-const app = express();
- 
-app.use(express.json());
-app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
-app.use(clerkMiddleware());
+import * as Sentry from "@sentry/node";
 
-app.get("/", (req, res) => {
-    res.json({ message: "Hello World" });
-});
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // req.auth will be available in the request object
 
 app.get("/debug-sentry", (req, res) => {
-    throw new Error("Sentry Debug Test Error");
+  throw new Error("My first Sentry error!");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World! 123");
 });
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
@@ -29,20 +31,19 @@ app.use("/api/chat", chatRoutes);
 Sentry.setupExpressErrorHandler(app);
 
 const startServer = async () => {
-    try {
-        await connectDB();
-        if (ENV.NODE_ENV !== "production") {
-            app.listen(ENV.PORT, () => {
-                console.log("Server is listening on http://localhost:", ENV.PORT)
-            })
-        }
-    } catch(error) {
-        console.error("Error starting the server: ", error);
-        process.exit(1);
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log("Server started on port:", ENV.PORT);
+      });
     }
+  } catch (error) {
+    console.error("Error starting server:", error);
+    process.exit(1); // Exit the process with a failure code
+  }
 };
 
 startServer();
 
 export default app;
-
